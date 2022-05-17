f457831d4fd70c8286f637254abed016d80bb4db
Collin Monahan <41239783+llaenowyd@users.noreply.github.com>
2022-05-16 21:41:49 -0400

    @a110/rito@0.0.4/DarkModeToggle (#1)

--- components/frontend/src/components/DarkModeToggle.jsx
+++ components/frontend/src/components/DarkModeToggle.jsx
@@ -1,88 +1,23 @@
-import React, { useCallback, useMemo, useState } from 'react'
+import React from 'react'
 import { useRecoilState } from 'recoil'
-import { Lock, Moon, Sun, Unlock } from 'lucide-react'
-import colors from 'tailwindcss/colors'
-import tw from 'twin.macro'
-import { SlideIn } from '@a110/rito'
+import { DarkModeToggle as RitoDarkModeToggle } from '@a110/rito'
 
-import { DARK_COLOR_SCHEME, LIGHT_COLOR_SCHEME } from '../constants'
 import { useColorScheme } from '../hooks/useColorScheme'
-import {
-  colorSchemeInheritSystemState,
-  colorSchemeState,
-} from '../recoil/settings'
-
-const Container = tw.div`
-  flex
-  p-2
-  gap-2
-  select-none
-  justify-end
-  border-2
-  border-transparent
-  hover:border-text-primary
-  rounded-xl
-`
+import { followDeviceColorSchemeState } from '../recoil/settings'
 
 const DarkModeToggle = () => {
-  const [isHovered, setIsHovered] = useState(false)
-  const actualColorScheme = useColorScheme()
-
-  const [onMouseEnter, onMouseLeave] = useMemo(
-    () => [
-      () => {
-        setIsHovered(true)
-      },
-      () => {
-        setIsHovered(false)
-      },
-    ],
-    [setIsHovered]
+  const { colorScheme, setColorScheme } = useColorScheme()
+  const [followDeviceColorScheme, setFollowDeviceColorScheme] = useRecoilState(
+    followDeviceColorSchemeState
   )
-
-  const [colorSchemeInheritSystem, setColorSchemeInheritSystem] =
-    useRecoilState(colorSchemeInheritSystemState)
-  const [colorScheme, setColorScheme] = useRecoilState(colorSchemeState)
-
-  const oneClickToggle = useCallback(() => {
-    const targetColorScheme =
-      DARK_COLOR_SCHEME === actualColorScheme
-        ? LIGHT_COLOR_SCHEME
-        : DARK_COLOR_SCHEME
-
-    if (targetColorScheme !== colorScheme) {
-      setColorScheme(targetColorScheme)
-    }
-
-    if (colorSchemeInheritSystem) {
-      setColorSchemeInheritSystem(false)
-    }
-  }, [actualColorScheme, colorScheme, colorSchemeInheritSystem])
-
-  // tbd recoil toggle
-  const toggleColorSchemeInheritSystem = useCallback(
-    () => setColorSchemeInheritSystem(!colorSchemeInheritSystem),
-    [colorSchemeInheritSystem, setColorSchemeInheritSystem]
-  )
-
-  const LockIcon = colorSchemeInheritSystem ? Lock : Unlock
-  const ColorSchemeIcon = DARK_COLOR_SCHEME === actualColorScheme ? Moon : Sun
 
   return (
-    <Container onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-      <SlideIn hidden={!isHovered}>
-        <LockIcon
-          style={{ cursor: 'pointer' }}
-          color={colors['text-primary']}
-          onClick={toggleColorSchemeInheritSystem}
-        />
-      </SlideIn>
-      <ColorSchemeIcon
-        style={{ cursor: 'pointer' }}
-        color={colors['text-primary']}
-        onClick={oneClickToggle}
-      />
-    </Container>
+    <RitoDarkModeToggle
+      colorScheme={colorScheme}
+      setColorScheme={setColorScheme}
+      followDevice={followDeviceColorScheme}
+      setFollowDevice={setFollowDeviceColorScheme}
+    />
   )
 }
 
