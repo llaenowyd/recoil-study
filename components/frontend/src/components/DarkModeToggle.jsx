897fc701bd71f59f25fe11ff3939976f73837c5f
Collin Monahan <llaenowyd@gmail.com>
2022-05-16 20:05:02 -0400

    @a110/rito@0.0.4/DarkModeToggle

--- components/frontend/src/components/DarkModeToggle.jsx
+++ components/frontend/src/components/DarkModeToggle.jsx
@@ -1,9 +1,6 @@
-import React, { useCallback, useMemo, useState } from 'react'
+import React, { useCallback } from 'react'
 import { useRecoilState } from 'recoil'
-import { Lock, Moon, Sun, Unlock } from 'lucide-react'
-import colors from 'tailwindcss/colors'
-import tw from 'twin.macro'
-import { SlideIn } from '@a110/rito'
+import { DarkModeToggle as RitoDarkModeToggle } from '@a110/rito'
 
 import { DARK_COLOR_SCHEME, LIGHT_COLOR_SCHEME } from '../constants'
 import { useColorScheme } from '../hooks/useColorScheme'
@@ -12,33 +9,8 @@ import {
   colorSchemeState,
 } from '../recoil/settings'
 
-const Container = tw.div`
-  flex
-  p-2
-  gap-2
-  select-none
-  justify-end
-  border-2
-  border-transparent
-  hover:border-text-primary
-  rounded-xl
-`
-
 const DarkModeToggle = () => {
-  const [isHovered, setIsHovered] = useState(false)
   const actualColorScheme = useColorScheme()
-
-  const [onMouseEnter, onMouseLeave] = useMemo(
-    () => [
-      () => {
-        setIsHovered(true)
-      },
-      () => {
-        setIsHovered(false)
-      },
-    ],
-    [setIsHovered]
-  )
 
   const [colorSchemeInheritSystem, setColorSchemeInheritSystem] =
     useRecoilState(colorSchemeInheritSystemState)
@@ -59,30 +31,13 @@ const DarkModeToggle = () => {
     }
   }, [actualColorScheme, colorScheme, colorSchemeInheritSystem])
 
-  // tbd recoil toggle
-  const toggleColorSchemeInheritSystem = useCallback(
-    () => setColorSchemeInheritSystem(!colorSchemeInheritSystem),
-    [colorSchemeInheritSystem, setColorSchemeInheritSystem]
-  )
-
-  const LockIcon = colorSchemeInheritSystem ? Lock : Unlock
-  const ColorSchemeIcon = DARK_COLOR_SCHEME === actualColorScheme ? Moon : Sun
-
   return (
-    <Container onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-      <SlideIn hidden={!isHovered}>
-        <LockIcon
-          style={{ cursor: 'pointer' }}
-          color={colors['text-primary']}
-          onClick={toggleColorSchemeInheritSystem}
-        />
-      </SlideIn>
-      <ColorSchemeIcon
-        style={{ cursor: 'pointer' }}
-        color={colors['text-primary']}
-        onClick={oneClickToggle}
-      />
-    </Container>
+    <RitoDarkModeToggle
+      colorScheme={actualColorScheme}
+      setColorScheme={oneClickToggle}
+      followDevice={colorSchemeInheritSystem}
+      setFollowDevice={setColorSchemeInheritSystem}
+    />
   )
 }
 
